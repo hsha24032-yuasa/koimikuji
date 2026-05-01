36ece98ce71e52805b3763efa13ac84abe5d869a
yuasa <yuasa@example.com>
2026-05-01 12:42:18 +0900

    結果でも文字色を変更

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ const result =document.getElementById("W");
 
 button.addEventListener("click",function(){
     const randomNumber=Math.floor(Math.random()*fortunes.length);
+　　const resultText = fortunes[(randomNumber)];
+
     result.textContent=fortunes[(randomNumber)];
+
+    if(resultText.includes("大吉")) {
+        result.style.color = "red";
+    }else if(resultText.includes("中吉")) {
+        result.style.color = "yellow";
+    }else  {
+        result.style.color = "black";
+    }
 });
